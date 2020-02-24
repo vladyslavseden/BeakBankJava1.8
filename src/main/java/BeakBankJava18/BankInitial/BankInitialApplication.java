e237277cdcb7cbc04058ff718026a7f1c6216770
John Doe <john@sample.com>
2020-02-24 13:15:28 +0200

    init Commit

---
+++ src/main/java/BeakBankJava18/BankInitial/BankInitialApplication.java
@@ -0,0 +1,13 @@
+package BeakBankJava18.BankInitial;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class BankInitialApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(BankInitialApplication.class, args);
+	}
+
+}
---
+++ src/test/java/BeakBankJava18/BankInitial/BankInitialApplicationTests.java
@@ -0,0 +1,13 @@
+package BeakBankJava18.BankInitial;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class BankInitialApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
